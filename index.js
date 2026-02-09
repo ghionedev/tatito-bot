42dee73c32b5a162863b1fad396262330e494665
ghionedev <ghionedev@gmail.com>
2026-02-09 11:25:58 -0300

    mvp

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-import express from "express";
-=======
 import { interpretMessage } from "./services/ia.js";
 import { saveReminder } from "./db/reminders.js";
 import { startDailyCheck } from "./scheluder/dailyCheck.js";
@@ -8,15 +5,10 @@ import { getLocalDateString } from "./utils/date.js";
 
 import express from "express";
 import 'dotenv/config';
->>>>>>> 9b981a7 (feat: add daily reminder scheduler and message builder)
 
 const app = express();
 const PORT = 3000;
 
-<<<<<<< HEAD
-app.get("/", (req, res) => {
-  res.send("Server is running 🚀");
-=======
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -49,15 +41,11 @@ app.post("/webhook/whatsapp", async (req, res) => {
       <Message>Perfecto 👍 Ya lo tengo anotado.</Message>
     </Response>
   `);
->>>>>>> 9b981a7 (feat: add daily reminder scheduler and message builder)
 });
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
-<<<<<<< HEAD
-=======
 
 startDailyCheck();
 
->>>>>>> 9b981a7 (feat: add daily reminder scheduler and message builder)
